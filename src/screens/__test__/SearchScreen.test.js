90a352269d7b5f923a417200dc015bf46fefc2c4
Mayur Kathale <mayur.kathale@gmail.com>
2023-06-16 04:21:07 +0530

    adding test case in unit test rendering

--- src/screens/__test__/SearchScreen.test.js
+++ src/screens/__test__/SearchScreen.test.js
@@ -2,7 +2,7 @@ import { act, fireEvent, render, screen } from '@testing-library/react-native';
 import StackNavigation from '../../navigation/stack';
 
 describe('Result Screen', () => {
-  it('should be rendered after input value in text of home screen', async () => {
+  it('should render table if searched for exising user', async () => {
     const navigation = { navigate: jest.fn() };
     render(<StackNavigation />);
     const input = await screen.getByTestId('searchText');
@@ -10,8 +10,24 @@ describe('Result Screen', () => {
     fireEvent.changeText(input, 'Emma');
     fireEvent.press(button);
     await act(async () => {
-      await new Promise((r) => setTimeout(r, 3000));
+      await new Promise(r => setTimeout(r, 3000));
       const text = screen.getByText('Emma');
+      expect(text).toBeTruthy();
+    });
+  });
+
+  it('should render error modal if searched for non-exising user', async () => {
+    const navigation = { navigate: jest.fn() };
+    render(<StackNavigation />);
+    const input = await screen.getByTestId('searchText');
+    const button = await screen.getByTestId('searchButton');
+    fireEvent.changeText(input, 'asdasdasd');
+    fireEvent.press(button);
+    await act(async () => {
+      await new Promise(r => setTimeout(r, 3000));
+      const text = screen.getByText(
+        'This user name does not exist! Please specify an existing user name!',
+      );
       expect(text).toBeTruthy();
     });
   });
